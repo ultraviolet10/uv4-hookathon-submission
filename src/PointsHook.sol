4725fc3b3d19480feafd4b5b1d60d6f5be5bcc4c
ultraviolet10 <aritrac1998@hotmail.com>
2026-06-01 18:16:27 +0530

    forge init

---
+++ src/PointsHook.sol
@@ -0,0 +1,105 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.26;
+ 
+// import {BaseHook} from "v4-hooks-public/src/base/BaseHook.sol";
+// import {ERC1155} from "solmate/src/tokens/ERC1155.sol";
+ 
+// import {Currency} from "v4-core/types/Currency.sol";
+// import {PoolKey} from "v4-core/types/PoolKey.sol";
+// import {PoolId} from "v4-core/types/PoolId.sol";
+// import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+// import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+ 
+// import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+ 
+// import {Hooks} from "v4-core/libraries/Hooks.sol";
+ 
+// contract PointsHook is BaseHook, ERC1155 {
+//     constructor(
+//         IPoolManager _manager
+//     ) BaseHook(_manager) {}
+ 
+// 	// Set up hook permissions to return `true`
+// 	// for the two hook functions we are using
+//     function getHookPermissions()
+//         public
+//         pure
+//         override
+//         returns (Hooks.Permissions memory)
+//     {
+//         return
+//             Hooks.Permissions({
+//                 beforeInitialize: false,
+//                 afterInitialize: false,
+//                 beforeAddLiquidity: false,
+//                 beforeRemoveLiquidity: false,
+//                 afterAddLiquidity: false,
+//                 afterRemoveLiquidity: false,
+//                 beforeSwap: false,
+//                 afterSwap: true,
+//                 beforeDonate: false,
+//                 afterDonate: false,
+//                 beforeSwapReturnDelta: false,
+//                 afterSwapReturnDelta: false,
+//                 afterAddLiquidityReturnDelta: false,
+//                 afterRemoveLiquidityReturnDelta: false
+//             });
+//     }
+ 
+//     // Implement the ERC1155 `uri` function
+//     function uri(uint256) public view virtual override returns (string memory) {
+//         return "https://api.example.com/token/{id}";
+//     }
+ 
+// 	// Stub implementation of `afterSwap`
+// 	function _afterSwap(
+//     address,
+//     PoolKey calldata key,
+//     SwapParams calldata swapParams,
+//     BalanceDelta delta,
+//     bytes calldata hookData
+//     ) internal override returns (bytes4, int128) {
+//         // If this is not an ETH-TOKEN pool with this hook attached, ignore
+//         if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
+    
+//         // We only mint points if user is buying TOKEN with ETH
+//         if (!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
+    
+//         // Mint points equal to 20% of the amount of ETH they spent
+//         // Since it's a zeroForOne swap:
+//         // if amountSpecified < 0:
+//         //      this is an "exact input for output" swap
+//         //      amount of ETH they spent is equal to |amountSpecified|
+//         // if amountSpecified > 0:
+//         //      this is an "exact output for input" swap
+//         //      amount of ETH they spent is equal to BalanceDelta.amount0()
+    
+//         uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
+//         uint256 pointsForSwap = ethSpendAmount / 5;
+    
+//         // Mint the points
+//         _assignPoints(key.toId(), hookData, pointsForSwap);
+    
+//         return (this.afterSwap.selector, 0);
+//     }
+
+//     function _assignPoints(
+//     PoolId poolId,
+//     bytes calldata hookData,
+//     uint256 points
+//     ) internal {
+//         // If no hookData is passed in, no points will be assigned to anyone
+//         if (hookData.length == 0) return;
+    
+//         // Extract user address from hookData
+//         address user = abi.decode(hookData, (address));
+    
+//         // If there is hookData but not in the format we're expecting and user address is zero
+//         // nobody gets any points
+//         if (user == address(0)) return;
+    
+//         // Mint points to the user
+//         uint256 poolIdUint = uint256(PoolId.unwrap(poolId));
+//         _mint(user, poolIdUint, points, "");
+//     }
+// }
